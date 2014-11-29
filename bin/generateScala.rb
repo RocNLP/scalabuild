76e5d7eb87c7ca47d5a01e6cc2e8317ebadfbe3a
mrmechko <thesoulshell@gmail.com>
2014-11-29 17:34:21 -0500

    still need command lineoptions and better way to load sbt templates

--- bin/generateScala.rb
+++ bin/generateScala.rb
@@ -6,4 +6,8 @@ require 'scalabuild'
 Scalabuild.make_dirs
 sbtVals = Hash.new
 
+sbtVals["project"] = "PROJECTNAME"
+sbtVals["versionNumber"] = "1.0"
+
 Scalabuild.writeSbt(sbtVals)
+Scalabuild.filler_files
