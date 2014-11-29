684303bea18349d8d4ca55faaf578fc9c4cecc61
mrmechko <thesoulshell@gmail.com>
2014-11-29 17:29:17 -0500

    just need to write an optioner for the bin

--- bin/generateScala.rb
+++ bin/generateScala.rb
@@ -6,6 +6,4 @@ require 'scalabuild'
 Scalabuild.make_dirs
 sbtVals = Hash.new
 
-sbtVals["project"] = "TestProject"
-sbtVals["versionNumber"] = "1.0"
 Scalabuild.writeSbt(sbtVals)
--- lib/scalabuild.rb
+++ lib/scalabuild.rb
@@ -24,15 +24,23 @@ module Scalabuild
 
   module_function
   def writeSbt(args) #lines that have a template that is not overridden are ignored.  As a switch, just include a key with empty value
+    out = File.open("build.sbt", 'w')
     File.open(File.dirname(__FILE__)+"/build.sbt.template", 'r') do |f|
       f.each_line do |line|
         keyVal = line.split("\#", 2)
 	if args[keyVal[0]] != nil
-          print keyVal[1].gsub(/\#\{#{keyVal[0]}\}/, args[keyVal[0]])
+          out.write( keyVal[1].gsub(/\#\{#{keyVal[0]}\}/, args[keyVal[0]])+"\n" )
 	end
       end
     end
-
   end
+
+  module_function
+  def filler_files
+    FileUtil.cp(File.dirname(__FILE__)+"/gitignore", ".gitignore")
+    FileUtil.cp(File.dirname(__FILE__)+"/README.md", "README.md")
+    FileUtil.cp(File.dirname(__FILE__)+"/LICENSE", "LICENSE")
+  end
+
 
 end
